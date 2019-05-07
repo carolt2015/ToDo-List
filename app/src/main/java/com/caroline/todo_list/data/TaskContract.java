c277307b1a8dc8c71df1ab5600102518261c5fea
Caroline <Caroline>
2019-05-06 23:54:45 -0700

    Initial commit

---
+++ app/src/main/java/com/caroline/todo_list/data/TaskContract.java
@@ -0,0 +1,35 @@
+package com.caroline.todo_list.data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class TaskContract {
+
+    public static final String AUTHORITY = "com.caroline.todo_list";
+
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+
+    public static final String PATH_TASKS = "tasks";
+
+
+    public static final class TaskEntry implements BaseColumns {
+
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_TASKS).build();
+
+
+
+        public static final String TABLE_NAME = "tasks";
+
+
+        public static final String COLUMN_DESCRIPTION = "description";
+        public static final String COLUMN_PRIORITY = "priority";
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/caroline/todo_list/data/TaskDbHelper.java
@@ -0,0 +1,35 @@
+package com.caroline.todo_list.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class TaskDbHelper extends SQLiteOpenHelper {
+
+
+    private static final String DATABASE_NAME = "tasksDb.db";
+
+
+    private static final int VERSION = 1;
+
+    TaskDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+
+        final String CREATE_TABLE = "CREATE TABLE " + TaskContract.TaskEntry.TABLE_NAME + " (" +
+                TaskContract.TaskEntry._ID + " INTEGER PRIMARY KEY, " +
+                TaskContract.TaskEntry.COLUMN_DESCRIPTION + " TEXT NOT NULL, " +
+                TaskContract.TaskEntry.COLUMN_PRIORITY + " INTEGER NOT NULL);";
+
+        db.execSQL(CREATE_TABLE);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TaskContract.TaskEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
